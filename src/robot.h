f85027649786ac8e2b5e13e48af25ebbac203ad8
Anton Schirg <anton.schirg@posteo.de>
2017-02-12 01:04:34 +0100

    abstract robot

---
+++ src/robot.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <cstdint>
+
+class Motor {
+public:
+    virtual void drive(int_16t speed) = 0;
+};
+
+class LineSensor {
+public:
+    virtual bool isLine() = 0;
+};
+
+class ColorLineSensor : public LineSensor {
+public:
+    virtual uint16_t getR() = 0;
+    virtual uint16_t getG() = 0;
+    virtual uint16_t getB() = 0;
+};
+
+class Robot {
+public:
+    Motor* motor_l;
+    Motor* motor_r;
+    LineSensor** line_sensors;
+};
